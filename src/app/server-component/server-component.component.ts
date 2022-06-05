cf5f771265b8425c277b88aa614990a7f5551372
jantejones <trebor.tejones@yahoo.com>
2022-06-04 22:34:09 -0400

    1 file modified, 4 added, 4 deleted

---
+++ src/app/server-component/server-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-component',
+  templateUrl: './server-component.component.html',
+  styleUrls: ['./server-component.component.css']
+})
+export class ServerComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+}
