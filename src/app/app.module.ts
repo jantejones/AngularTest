2d7d067ea273b70eca22550debc5891d6c2e4ad0
jantejones <trebor.tejones@yahoo.com>
2022-06-04 22:10:25 -0400

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, WarningAlertComponent, SuccessAlertComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
